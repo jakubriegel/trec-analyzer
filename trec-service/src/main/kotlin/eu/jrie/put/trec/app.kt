7948a185f0bbaf9c74a86aa0e3f9e40aa86df0cf
jakubriegel <jakubriegel112@gmail.com>
2021-01-05 20:07:55 +0100

    verbose env

--- trec-service/src/main/kotlin/eu/jrie/put/trec/app.kt
+++ trec-service/src/main/kotlin/eu/jrie/put/trec/app.kt
@@ -2,6 +2,7 @@ package eu.jrie.put.trec
 
 import eu.jrie.put.trec.api.startServer
 import eu.jrie.put.trec.domain.index.initEs
+import eu.jrie.put.trec.domain.index.initTerrier
 import eu.jrie.put.trec.infra.env
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
@@ -19,12 +20,7 @@ fun main() {
 @ObsoleteCoroutinesApi
 @FlowPreview
 fun app() {
-    if (env("TREC_INIT_INDEX") == "1") initIndexes()
+    if (env("TREC_INIT_TERRIER") == "1") initTerrier()
+    if (env("TREC_INIT_ES") == "1") initEs()
     if (env("TREC_SERVER") == "1") startServer()
-}
-
-@ExperimentalCoroutinesApi
-fun initIndexes() {
-//    initTerrier()
-    initEs()
 }
